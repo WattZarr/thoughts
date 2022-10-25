4f92c6f1f485c8f06e0e008a58007695beeb4615
WattZarr <lwinkoko22801@gmail.com>
2022-10-25 12:30:59 +0630

    solve error

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -49,7 +49,7 @@ export default function Dashboard(){
             <h3>
                 {posts.map((post)=>{
                     return (
-                    <Message {...post}>
+                    <Message {...post} key={post.id}>
                         <div className='flex gap-4 mt-4'>
                           <button onClick={()=>deletePost(post.id)} className='flex gap-2 py-2 px-3 rounded-lg items-center border-solid border-2 border-red-500 text-red-500'>
                             <BsTrash2Fill/>Delete
